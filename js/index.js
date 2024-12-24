40c2f9e5a07464cfef19579b98416f0004feacc5
yOriginTwo <orlsgabriel09@gmail.com>
2024-12-24 15:12:46 -0300

    Versão 1.2.1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// var = re-declaravel. , let = declaração unica, obdece ao scopo do site. , const = constante, ou seja o valor colocado nela, não vai mudar.
+/* # Um Helpzinho.
+#
+#  ---- Variaveis ----
+#
+#  Var = Variavel que seu valor pode ser modificado facilmente em qualquer parte do codigo.
+#  Let = Variavel que seu valor é modificado somente em situações especificas, como if e else.
+# Const = Variavel que seu valor nunca sera modificavel.
+#
+#  ----  Função  ----
+# // O Nome da função nao pode haver: acentos, (-), (_) ou (.)
+# // O Valor da função serve como a variavel utilizada pra realizar a função, veja o exemplo abaixo. //
+# // A Função pode ser feita sem valores também. //
+#
+#    function NomeDaFunçao(valor1,valor2){
+#       return valor1 * valor2;
+#       }
+#
+# // Essa função é um exemplo de soma, voce pode ver como utilizamos ela abaixo. //
+# // Usando de exemplo o alert para enviar ao usuario os dados recebidos. //
+#
+# // O Dado enviado sera o numero 2, porque 1x2 = 2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          . //
+#
+#     alert(NomeDaFuncao(1,2));
+#
+# ---- XXXXXX ----
+*/
+
